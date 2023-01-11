5f56741081c26bf01a8215ce1f0b0add62a6d5f6
akshu1 <akshathanagaraj98@gmail.com>
2023-01-11 06:28:35 +0530

    Add:Program to calculate employee total wages till condition is met Using While

--- src/main/java/com/bridgelabz/employeewagecomputation/TotalWagesTillConditionMetUsingWhile.java
+++ src/main/java/com/bridgelabz/employeewagecomputation/TotalWagesTillConditionMetUsingWhile.java
@@ -1,17 +1,18 @@
 package com.bridgelabz.employeewagecomputation;
 
-public class EmployeeWagesMonthly {
+public class TotalWagesTillConditionMetUsingWhile {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
     public static void main(String[] args) {
 
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
             switch (empCheck) {
@@ -24,11 +25,11 @@ public class EmployeeWagesMonthly {
                 default:
                     empHrs = 0;
             }
+            totalEmpHrs += empHrs;
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println(" Emp Wage: " + empWage);
+            System.out.println("Days#: " + totalWorkingDays  +   "Emp Hr:" + empHrs);
         }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wages: " + totalEmpWage);
 
     }
